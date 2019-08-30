9845b34666847eb1eff5c667274ae2203a0be54d
chengkangzai <pycck@hotmail.com>
2019-08-30 15:45:37 +0800

    update link

--- js/pre_load.js
+++ js/pre_load.js
@@ -1,5 +1,31 @@
 var overlay = document.getElementById("overlay");
 
 window.addEventListener('load', function() {
-    overlay.style.display = 'none';
-})
+        overlay.style.display = 'none';
+    })
+    /*
+    var age = 40
+    if (age <= 0) {
+        alert("Please Alive")
+    } else if (age <= 2) {
+        alert("Chin Wei is a Infrant")
+    } else if (age <= 12) {
+        alert("Chin Wei is a Kid")
+    } else if (age <= 20) {
+        alert("Chin Wei is a Teen")
+    } else if (age <= 60) {
+        alert("Chin Wei is a Adult")
+    } else if (age > 60) {
+        alert("Chin Wei is a Kid")
+    }*/
+
+var age = "child"
+
+switch (age) {
+    case "child":
+        alert("sss")
+        break;
+
+    default:
+        break;
+}
